ea8fa96259a6b92be8fbcc06e1ff0f41b0603cec
Aris <akoumis1@gmail.com>
2017-03-29 11:37:56 -0700

    Added collapsible view in RestaurantScreen

--- components/RestaurantListElement.js
+++ components/RestaurantListElement.js
@@ -6,33 +6,56 @@ import {
 	Image,
 	StyleSheet
 } from 'react-native';
+import Collapsible from 'react-native-collapsible';
+import Icon from 'react-native-vector-icons/Foundation';
 
 export default class RestaurantListElement extends React.Component {
 	constructor(props) {
 		super(props)
-		console.log(props)
-		this.handleRegionUpdate = this.handleRegionUpdate.bind(this)
+		this.state = {
+			collapsed: true
+		}
+		this.handleFocus = this.handleFocus.bind(this)
 	}
 
 	render() {
 		return (
-			<TouchableOpacity onPress={this.handleRegionUpdate} style={styles.container}>
-				<Image style={styles.image} source={{uri: this.props.restaurant.image_url}} />
-				<View style={styles.info}>
-					<Text style={{fontWeight: "bold", fontSize: 14}}>{this.props.restaurant.name}</Text> 
-					<Text style={{fontSize: 12}}>{this.props.restaurant.categories[0].title} - {this.props.restaurant.price} </Text> 
-				</View>
-			</TouchableOpacity>
+			<View>
+				<TouchableOpacity onPress={this.handleFocus} style={styles.container}>
+					<Image style={styles.image} source={{uri: this.props.restaurant.image_url}} />
+					<View style={styles.info}>
+						<Text style={{fontWeight: "bold", fontSize: 14}}>{this.props.restaurant.name}</Text> 
+						<Text style={{fontSize: 12}}>{this.props.restaurant.categories[0].title} - {this.props.restaurant.price} </Text> 
+					</View>
+				</TouchableOpacity>
+		        <Collapsible collapsed={this.state.collapsed} align="center">
+		        	<View style={styles.collapsible}>
+		        		<View style={styles.collapsibleItem}>
+		        			<Icon size={30} name="map" />
+		        			<Text> Directions </Text>
+	        			</View>
+	        			<View style={styles.collapsibleItem}>
+		        			<Icon size={30} name="telephone" />
+		        			<Text> Call </Text>
+	        			</View>
+	        			<View style={styles.collapsibleItem}>
+		        			<Icon size={30} name="web" />
+		        			<Text> Website </Text>
+	        			</View>
+	        		</View>
+		        </Collapsible>
+	        </View>
 		)
 	}
 
-	handleRegionUpdate() {
+	handleFocus() {
 		var newRegion = {
 			longitude: this.props.restaurant.coordinates.longitude,
 			latitude: this.props.restaurant.coordinates.latitude,
 			latitudeDelta: 0.0922,
           	longitudeDelta: 0.0421,
 		}
+		this.setState({collapsed: !this.state.collapsed})
 		this.props.handleRegionUpdate(newRegion)
 	}
 }
@@ -47,9 +70,17 @@ const styles = StyleSheet.create({
 		flexDirection: 'column',
 		paddingLeft: 5
 	},
-	image : {
+	image: {
 		height:50, 
 		width:50,
 		borderRadius:5
+	},
+	collapsible: {
+		flexDirection: 'row',
+		justifyContent: 'space-around'
+	},
+	collapsibleItem: {
+		flexDirection: "column",
+		alignItems: "center"
 	}
 })
